85b5351e96c73f4b2c772abb5ca3cae0f35eb871
MayankSinghChouhan <email.mayanksinghchouhan@gmail.com>
2022-08-13 16:14:05 +0530

    Added logic for updating notes on edit in the UI

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -8,7 +8,7 @@ import AddNote from './AddNote';
 const Notes = () => {
 
     const context = useContext(noteContext);
-    const { notes, getNotes } = context;
+    const { notes, getNotes, editNote } = context;
 
     useEffect(() => {
         getNotes()
@@ -16,17 +16,18 @@ const Notes = () => {
     }, [])
 
     const ref = useRef(null)
+    const refClose = useRef(null)
 
-    const [note, setNote] = useState({ etitle: "", edescription: "", etag: "" });
+    const [note, setNote] = useState({ id: "", etitle: "", edescription: "", etag: "" });
 
     const updateNote = (currentNote) => {
         ref.current.click();
-        setNote({ etitle: currentNote.title, edescription: currentNote.description, etag: currentNote.tag });
+        setNote({ id: currentNote._id, etitle: currentNote.title, edescription: currentNote.description, etag: currentNote.tag });
     }
 
     const handleClick = (e) => {
-        console.log("updating the note...", note)
-        e.preventDefault();
+        editNote(note.id, note.etitle, note.edescription, note.etag);
+        refClose.current.click();
     }
 
     const onChange = (e) => {
@@ -77,7 +78,7 @@ const Notes = () => {
 
                         </div>
                         <div className="modal-footer">
-                            <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                            <button ref={refClose} type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
                             <button onClick={handleClick} type="button" className="btn btn-primary">Update Note</button>
                         </div>
                     </div>
--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -34,6 +34,9 @@ const NoteState = (props) => {
       body: JSON.stringify({ title, description, tag })
     });
 
+    const json = await response.json();
+    console.log(json)
+
     console.log("Adding a new note")
     const note = {
       "_id": "62efehhjjjjjjhhh1ac4e62a8a8ac1cke1e",
@@ -73,24 +76,29 @@ const NoteState = (props) => {
 
     // API Call
     const response = await fetch(`${host}/api/notes/updatenote/${id}`, {
-      method: 'POST',
+      method: 'PUT',
       headers: {
         'Content-Type': 'application/json',
         'auth-token': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjJlZjNiZjE3Yzg2YTk4Y2IwNmFiM2FjIn0sImlhdCI6MTY1OTg2NzIwNX0.dZuQnQPaEZvdtlApo3X6-w3DjVPQPdbY80471KTJ5o0'
       },
       body: JSON.stringify({ title, description, tag })
     });
-    const json = response.json();
+    const json = await response.json();
+    console.log(json);
 
+    let newNotes = JSON.parse(JSON.stringify(notes))
     // Logic to edit in client
-    for (let index = 0; index < notes.length; index++) {
-      const element = notes[index];
+    for (let index = 0; index < newNotes.length; index++) {
+      const element = newNotes[index];
       if (element._id === id) {
-        element.title = title;
-        element.description = description;
-        element.tag = tag;
+        newNotes[index].title = title;
+        newNotes[index].description = description;
+        newNotes[index].tag = tag;
+
+        break;
       }
     }
+    setNotes(newNotes);
 
   }
 
